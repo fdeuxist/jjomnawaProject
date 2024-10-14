78cd11fa35c85f19477d37c8f9de0f2f509865ce
fdeuxist <fdeuxist@naver.com>
2024-10-14 11:56:18 +0900

    쫌나와 프로젝트 시작

---
+++ jjomnawaProject/src/main/java/com/example/jjomnawaProject/JjomnawaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jjomnawaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjomnawaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjomnawaProjectApplication.class, args);
+	}
+
+}
---
+++ jjomnawaProject/src/test/java/com/example/jjomnawaProject/JjomnawaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jjomnawaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjomnawaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
